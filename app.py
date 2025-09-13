d73b403b8d5875e872513a33b9633bc519b2f88b
adil-km <adilkmparappur@gmail.com>
2025-09-13 17:07:09 +0530

    file uploading and retrieving is set

--- app.py
+++ app.py
@@ -18,23 +18,51 @@ def allowed_file(filename):
 def home():
     return render_template("home.html")
 
+@app.route('/converted', methods=["GET"])
+def download():
+    return render_template("convert.html")
+
 @app.route('/pdf', methods=['GET', 'POST'])
 def upload_pdf():
     if request.method == "POST":
-        print("pdf")
+        if "pdfFile" in request.files:
+            file = request.files["pdfFile"]
+            if file.filename != '':
+                filename = secure_filename(file.filename)
+                upload_dir = 'static/uploads'
+                os.makedirs(upload_dir, exist_ok=True)
+                file_path = os.path.join(upload_dir, filename)
+                file.save(file_path)
+                return "File saved successfully"
+        return "No pdf file provide"
     return render_template("pdf-upload.html")
 
 @app.route('/image', methods=['GET', 'POST'])
 def upload_img():
     if request.method == "POST":
-        print("image")
+        if "imageFile" in request.files:
+            file = request.files["imageFile"]
+            if file.filename != '':
+                filename = secure_filename(file.filename)
+                upload_dir = 'static/uploads'
+                os.makedirs(upload_dir, exist_ok=True)
+                file_path = os.path.join(upload_dir, filename)
+                file.save(file_path)
+                return "File saved successfully"
+        return "No image provide"
     return render_template("image-upload.html")
 
 @app.route('/text', methods=['GET', 'POST'])
 def upload_txt():
     if request.method == "POST":
-        print("text")
+        res= request.form["textData"]
+        return res
     return render_template("text-upload.html")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
